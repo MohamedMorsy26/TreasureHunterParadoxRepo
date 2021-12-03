b405d038a49419fc929cfaa993f00d32d2c79f52
Mohamed Morsy <57285056+MohamedMorsy26@users.noreply.github.com>
2021-12-03 04:06:29 +0200

    Added PreSecondChoiceTimeline
    with transitions and all
    buggy at the end though...
    loop doesn't work so models get sucked into the floor...

--- TreasureHunterParadox/Assets/GameManagerScript.cs
+++ TreasureHunterParadox/Assets/GameManagerScript.cs
@@ -5,17 +5,21 @@ using UnityEngine.Playables;
 
 public class GameManagerScript : MonoBehaviour
 {
+    //Timelines
     public PlayableDirector IntroTimeLine;
     public PlayableDirector KickDoorTimeLine;
-    public GameObject OptionOneCamera;
+    public PlayableDirector PushButtonTimeLine;
+    public PlayableDirector PreChoiceTwoTimeLine;
+
+    //Cameras & Canvases
     public GameObject MainCamera;
-    public GameObject AfterFirstChoiceCamera;
+    public GameObject OptionOneCamera;
     public GameObject OptionOneCanvas;
+    public GameObject AfterFirstChoiceCamera;
+    public GameObject PreChoiceTwoCamera;
+    public GameObject PreChoiceTwoCanvas;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Start(){}
 
     // Update is called once per frame
     void Update()
@@ -41,5 +45,24 @@ public class GameManagerScript : MonoBehaviour
     public void StartKickDoorTimeLine() {
         OptionOneCanvas.SetActive(false);
         KickDoorTimeLine.Play();
+    }
+    public void StartPushButtonTimeLine() {
+        OptionOneCanvas.SetActive(false);
+        PushButtonTimeLine.Play();
+    }
+    public void StartPreChoiceTwoTimeLine()
+    {
+        PreChoiceTwoTimeLine.Play();
+        PreChoiceTwoCamera.SetActive(true);
+        AfterFirstChoiceCamera.SetActive(false);
+    }
+    public void ChoiceTwoInitiate() {
+        PreChoiceTwoCanvas.SetActive(true);
+    }
+    public void StartKillEveryoneTimeLine() {
+        
+    }
+    public void StartSaveErikaTimeLine(){
+
     }
 }
